a0e745a36cb3266b4fe2d2fb1305e926eef9165a
Leandro Fogaça <fogaca.lf@gmail.com>
2023-01-19 20:16:06 -0300

    text-align

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import CardProfile from "./components/CardProfile"
+import Bio from "./components/Bio"
+import Name from "./components/Name"
 
 
 function App() {
-    return <CardProfile />
+    return <div>
+        < Name name="Leandro Fogaça" />
+        < Bio text="Essa é uma página para apresentar meus projetos e os primeiros passos de CSS" />
+    </div>
 }
 
 export default App

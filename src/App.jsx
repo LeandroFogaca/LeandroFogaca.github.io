5707528cc0e03cc7696e961469aef94bb3661728
Leandro Fogaça <fogaca.lf@gmail.com>
2023-01-24 11:15:40 -0300

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import CardProfile from "./components/CardProfile"
-
+import Footer from "./components/Footer"
 
 function App() {
     return <div>
         < CardProfile />
+        <Footer />
     </div>
 }
 
--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -7,7 +7,13 @@ function CardProfile() {
         <div className="card">
             < Picture />
             < Name name="Leandro Fogaça" />
-            < Bio text="Essa é uma página para apresentar meus projetos e os primeiros passos de CSS" />
+            < Bio text="Está página está em construção, 
+            o objetivo é aprender um pouco sobre CSS e futuramente
+            será organizado e demostrado os diversos projetos em que atuo,
+            Comecei minha carreira como eletricista, depois me especializei em
+            manutenção industrial, sempre me interessei por automação e
+            desenvolvimento de hardwares e softwares. Atualmente estudo JavaScript,
+            um pouco de Python e Lua." />
         </div>
     )
 }
